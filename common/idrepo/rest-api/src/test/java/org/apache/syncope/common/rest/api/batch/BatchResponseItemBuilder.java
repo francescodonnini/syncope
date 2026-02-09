2e34db5f0cade25f6cba2f72a27d2fa8a1f066e8
francesco <donnini.francesco00@gmail.com>
2026-02-09 15:50:55 +0100

    aggiunta classe per costruire più semplicemente risposte http

---
+++ common/idrepo/rest-api/src/test/java/org/apache/syncope/common/rest/api/batch/BatchResponseItemBuilder.java
@@ -0,0 +1,51 @@
+package org.apache.syncope.common.rest.api.batch;
+
+import java.util.ArrayList;
+
+public class BatchResponseItemBuilder {
+    private final BatchResponseItem item = new BatchResponseItem();
+    private final StringBuilder content = new StringBuilder();
+
+    public static BatchResponseItemBuilder builder() {
+        return new BatchResponseItemBuilder();
+    }
+
+    private BatchResponseItemBuilder() {
+    }
+
+    public BatchResponseItemBuilder status(int status) {
+        item.setStatus(status);
+        return this;
+    }
+
+    public BatchResponseItemBuilder header(String key, String value) {
+        item.getHeaders().computeIfAbsent(key, k -> new ArrayList<>()).add(value);
+        return this;
+    }
+
+    public BatchResponseItemBuilder content(String content) {
+        item.setContent(content);
+        return this;
+    }
+
+    public BatchResponseItemBuilder line(String line) {
+        return line(line, false);
+    }
+
+    public BatchResponseItemBuilder line(String line, boolean unixEol) {
+        content.append(line);
+        if (unixEol) {
+            content.append("\n");
+        } else {
+            content.append("\r\n");
+        }
+        return this;
+    }
+
+    public BatchResponseItem create() {
+        if (!content.isEmpty()) {
+            item.setContent(content.toString());
+        }
+        return item;
+    }
+}
