26c45416ea51ba957b90cec02e47ed4a3533b16c
francesco <donnini.francesco00@gmail.com>
2026-04-18 02:37:25 +0200

    import checkstyle issue

--- common/idrepo/rest-api/src/test/java/org/apache/syncope/common/rest/api/batch/BatchPayloadParserTest.java
+++ common/idrepo/rest-api/src/test/java/org/apache/syncope/common/rest/api/batch/BatchPayloadParserTest.java
@@ -13,6 +13,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
