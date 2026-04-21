80f2f3fe35ef5c2ba479031fbca067a51ce6cf12
francesco <donnini.francesco00@gmail.com>
2026-04-21 22:40:13 +0200

    some tests

--- common/idrepo/rest-api/src/test/java/org/apache/syncope/common/rest/api/batch/BatchPayloadParserTest.java
+++ common/idrepo/rest-api/src/test/java/org/apache/syncope/common/rest/api/batch/BatchPayloadParserTest.java
@@ -1002,4 +1002,33 @@ public class BatchPayloadParserTest {
         Assertions.assertEquals(expected.getHeaders(), actual.getHeaders());
         Assertions.assertEquals(expected.getContent(), actual.getContent());
     }
+
+    @Test
+    public void testMissingCR() throws IOException {
+        byte[] data = ("--bnd\r\n"
+                + "some\n"
+                + "    data\n"
+                + "--bnd\n   "
+                + " a bdiod\r\n"
+                + "--bnd--").getBytes();
+        List<BatchRequestItem> batches = BatchPayloadParser.parse(new ByteArrayInputStream(data), mediaType("bnd"), new BatchRequestItem());
+        Assertions.assertEquals(2, batches.size());
+    }
+
+    @Test
+    public void testRequestResponseMismatch() throws IOException {
+        String boundary = "padded_boundary";
+        byte[] payload = BatchPayloadBuilder.builder().boundary(boundary)
+                .delimiter(5)
+                .line()
+                .line("Content-Type: application/http")
+                .line("Content-Transfer-Encoding: binary")
+                .line()
+                .line("POST /users HTTP/1.1")
+                .line("Accept: application/json")
+                .closingDelimiter(10)
+                .create();
+        List<BatchResponseItem> items = BatchPayloadParser.parse(new ByteArrayInputStream(payload), mediaType(boundary), new BatchResponseItem());
+        Assertions.assertEquals(1, items.size());
+    }
 }
